422b3603d6d89019c143ea9cc43fe673d36eaae9
Hans Sowa <hans.sowa@givval.com>
2021-10-13 13:36:49 +0200

    Introduce skeleton for writing rest based services

---
+++ src/main/java/at/fh/move/letsmove/LetsmoveApplication.java
@@ -0,0 +1,13 @@
+package at.fh.move.letsmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsmoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsmoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fh/move/letsmove/LetsmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fh.move.letsmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsmoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
